a45c261c1bcf0885e0bd2cd6043eb950cbf8d738
Alexey Lizurchik <al.lizurchik@gmail.com>
2025-09-08 00:01:47 +0200

    add navigation link fetching and merging to getPage function

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -1,10 +1,46 @@
 import { ExtendedRecordMap, SearchParams, SearchResults } from 'notion-types';
-import { isPreviewImageSupportEnabled } from './config';
+import { mergeRecordMaps } from 'notion-utils';
+import pMap from 'p-map';
+import pMemoize from 'p-memoize';
+import { isPreviewImageSupportEnabled, navigationLinks } from './config';
 import { notionApi } from './notionApi';
 import { getPreviewImageMap } from './previewImages';
 
+const getNavigationLinkPages = pMemoize(
+  async (): Promise<ExtendedRecordMap[]> => {
+    const navigationLinkPageIds = (navigationLinks || [])
+      .map(link => link?.pageId)
+      .filter(Boolean);
+
+    return pMap(
+      navigationLinkPageIds,
+      async navigationLinkPageId =>
+        notionApi.getPage(navigationLinkPageId, {
+          chunkLimit: 1,
+          fetchMissingBlocks: false,
+          fetchCollections: false,
+          signFileUrls: false,
+        }),
+      {
+        concurrency: 4,
+      },
+    );
+  },
+);
+
 export async function getPage(pageId: string): Promise<ExtendedRecordMap> {
-  const recordMap = await notionApi.getPage(pageId);
+  let recordMap = await notionApi.getPage(pageId);
+
+  const navigationLinkRecordMaps = await getNavigationLinkPages();
+
+  // query each navigation link page and merge in the record map in order to always have data about them
+  if (navigationLinkRecordMaps?.length) {
+    recordMap = navigationLinkRecordMaps.reduce(
+      (map, navigationLinkRecordMap) =>
+        mergeRecordMaps(map, navigationLinkRecordMap),
+      recordMap,
+    );
+  }
 
   if (isPreviewImageSupportEnabled) {
     const previewImageMap = await getPreviewImageMap(recordMap);
